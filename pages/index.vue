177386ba6bc3894c4b8ca1cc607ae9bb5de16b3f
橋本範昭 <nori.hasimo.89@gmail.com>
2021-06-30 18:58:50 +0900

    コンポーネント復習

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="container">
     <div class="number">{{ count }}</div>
-    <!-- <v-btn class="mx-1" fab dark @click="plus" color="pink">
-      <v-icon dark> mdi-plus </v-icon>
+    <!-- <button class="button" @click="plus">+</button>
+    <button class="button" @click="minus">-</button> -->
+
+    <!-- <v-btn class="mx-1" fab dark color="yellow" @click="plus">
+      <v-icon dark>mdi-plus</v-icon>
     </v-btn>
 
-    <v-btn class="mx-1" fab dark @click="minus" color="yellow">
-      <v-icon dark> mdi-minus </v-icon>
+    <v-btn class="mx-1" fab dark color="blue" @click="minus">
+      <v-icon dark>mdi-minus</v-icon>
+    </v-btn>
+
+    <v-btn class="mx-2" fab dark large color="cyan">
+      <v-icon dark> mdi-pencil </v-icon>
     </v-btn> -->
 
-    <counter-button kind="plus" ,color="pink" @click="plus" />
-    <counter-button kind="minus" ,color="yellow" @click="minus" />
+    <counter-button kind="plus" color="yellow" @push="plus" />
+    <counter-button kind="minus" color="blue" @push="minus" />
   </div>
 </template>
 
@@ -21,6 +28,7 @@ export default {
   components: {
     CounterButton,
   },
+
   data() {
     return {
       count: 100,
@@ -28,64 +36,72 @@ export default {
   },
   methods: {
     plus() {
-      this.count += 1
+      this.count += 10
     },
     minus() {
-      this.count -= 1
+      this.count -= 10
     },
   },
 }
 </script>
 
 <style lang="scss">
-.container {
-  .number {
-    font-size: 100px;
-    color: white;
-  }
+.number {
+  font-size: 80px;
 }
+
+// .button {
+//   font-size: 40px;
+//   font-weight: 30px;
+//   border: 2px solid gray;
+//   width: 60px;
+//   height: 60px;
+//   border-radius: 6px;
+// }
 </style>
+
 <!-- <div class="container"> -->
+
 <!-- <div class="number">{{ count }}</div> -->
 
 <!-- <v-btn class="mx-1" fab dark color="green" @click="plus">
-      <v-icon dark> mdi-plus</v-icon>
-    </v-btn>
+//       <v-icon dark> mdi-plus</v-icon>
+//     </v-btn>
 
-    <v-btn class="mx-1" fab dark color="red" @click="minus">
-      <v-icon dark> mdi-minus</v-icon>
-    </v-btn> -->
+//     <v-btn class="mx-1" fab dark color="red" @click="minus">
+//       <v-icon dark> mdi-minus</v-icon>
+//     </v-btn> -->
 
 <!-- <counter-button kind="plus" color="blue" @click="plus" />
-    <counter-button kind="minus" color="orange" @click="minus" />
-  </div>
-</template> -->
+//     <counter-button kind="minus" color="orange" @click="minus" />
+//   </div>
+// </template> -->
 
 <!-- <script>
-import CounterButton from '@/components/CounterButton.vue'
+// import CounterButton from '@/components/CounterButton.vue'
 
-export default {
-  components: {
-    CounterButton,
-  },
+// export default {
+//   components: {
+//     CounterButton,
+//   },
 
-  data() {
-    return {
-      count: 100,
-    }
-  },
-  methods: {
-    plus() {
-      this.count += 1
-    },
-    minus() {
-      this.count -= 1
-    },
-  },
-}
-</script>
+//   data() {
+//     return {
+//       count: 100,
+//     }
+//   },
+//   methods: {
+//     plus() {
+//       this.count += 1
+//     },
+//     minus() {
+//       this.count -= 1
+//     },
+//   },
+// }
+// </script>
 
-<style lang="scss" module>
+// <style lang="scss" module>
 // .container {
 //   .number {
 //     font-size: 70px;
